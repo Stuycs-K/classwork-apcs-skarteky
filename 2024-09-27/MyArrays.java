51d6740dbd046f8b55437e15c31dc8df31eccd09
Selina Lin <selinalin2178@gmail.com>
2024-09-27 11:43:49 -0400

    added arrayToString and test case for it as well as start of test cases for returnCopy and concatArray

---
+++ 2024-09-27/MyArrays.java
@@ -0,0 +1,21 @@
+public class MyArrays {
+  public static void main(String args[]) {
+    int[] test = {1,2,3};
+    int[] test 2 = {4,5.6};
+System.out.println(arrayToString(test));
+
+System.out.println("Is returnCopy(test) different from test: " + (returnCopy(test) != test));
+System.out.println("Did concatArray(test,test2) combine both: " + (concatArray(test, test2)));
+  }
+
+  public static String arrayToString(int[] nums) {
+    String result = "[";
+    for(int i = 0; i < nums.length; i++) {
+      result = result + nums[i];
+      if (i != nums.length - 1) {
+        result = result + ", ";
+      }
+    }
+    return result + "]";
+  }
+}
