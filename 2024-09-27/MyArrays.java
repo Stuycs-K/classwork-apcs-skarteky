44ad56fc0c4ceee6b10a235c39728340f7fd306e
Selina Lin <selinalin2178@gmail.com>
2024-09-29 04:57:23 -0400

    fixed test case for concatArray

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -4,8 +4,11 @@ public class MyArrays {
     int[] test2 = {4,5,6};
 System.out.println(arrayToString(test));
 
-System.out.println("Is returnCopy(test) different from test: " + (returnCopy(test) != test));
-System.out.println("Did concatArray(test,test2) combine both: " + (arrayToString(concatArray(test, test2)).equals(arrayToString(test).substring(0,test.length-1)+ ", " + arrayToString(test2).substring(1))));
+
+System.out.println("Original: " + arrayToString(test) + " Copy: " + arrayToString(returnCopy(test)) +
+"Is returnCopy(test) different from test: " + (returnCopy(test) != test));
+System.out.println("New ary: " + arrayToString(concatArray(test,test2)) + 
+" Did concatArray(test,test2) combine both: " + (arrayToString(concatArray(test, test2)).equals(arrayToString(test).substring(0,(arrayToString(test)).length()-1)+ ", " + arrayToString(test2).substring(1))));
   }
 
   public static String arrayToString(int[] nums) {
