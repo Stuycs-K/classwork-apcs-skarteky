63cb401ea645eae14c542e63003d4925dd97b5c5
Selina Lin <selinalin2178@gmail.com>
2024-09-29 04:37:15 -0400

    added to concatArray and returnCopy

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,11 +1,11 @@
 public class MyArrays {
   public static void main(String args[]) {
     int[] test = {1,2,3};
-    int[] test 2 = {4,5.6};
+    int[] test2 = {4,5,6};
 System.out.println(arrayToString(test));
 
 System.out.println("Is returnCopy(test) different from test: " + (returnCopy(test) != test));
-System.out.println("Did concatArray(test,test2) combine both: " + (arrayToString(concatArray(test, test2)).equals(arrayToString(test).substring(0,test.length-2)+ ", " + arrayToString(test2).substring(1)) );
+System.out.println("Did concatArray(test,test2) combine both: " + (arrayToString(concatArray(test, test2)).equals(arrayToString(test).substring(0,test.length-1)+ ", " + arrayToString(test2).substring(1))));
   }
 
   public static String arrayToString(int[] nums) {
@@ -17,5 +17,24 @@ System.out.println("Did concatArray(test,test2) combine both: " + (arrayToString
       }
     }
     return result + "]";
+  }
+  
+  public static int[] returnCopy(int[]ary) {
+	int[] result = new int[ary.length];
+	for(int i = 0; i < ary.length; i++) {
+		result[i] = ary[i];
+	}
+	return result;
+  }
+  
+  public static int[] concatArray(int[]ary1,int[]ary2) {
+	  int result = new int[ary1.length + ary2.length];
+	  for (int i = 0; i < ary1.length; i++) {
+		  result[i] = ary1[i];
+	  }
+	  for (int j = 0; j < ary2.length; j++) {
+		  result[ary1.length + j] = ary2[j];
+	  }
+	  return result;
   }
 }
