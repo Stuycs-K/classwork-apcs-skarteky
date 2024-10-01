1516863b550b5855eb00a836b2cddcc78625cbcf
Selina Lin <selinalin2178@gmail.com>
2024-10-01 00:22:25 -0400

    added to body of arrToString and arr2DSum

--- 2024-09-30/ArrayMethods.java
+++ 2024-09-30/ArrayMethods.java
@@ -1,13 +1,13 @@
 public class ArrayMethods{
   /* Kevin Li kevinl687@nycstudents.net
-     Selina Lin selinal50@nyystem.out.println("Expected [[10, 15, 8, -3, 0], []] " + arrToString({10, 15, 8, -3, 0}, {}));cstudents.net */
+     Selina Lin selinal50@nycstudents.net */
   public static void main(String args[]) {
     System.out.println("Expected [[1, 2], [3]] " + arrToString({1,2}, {3}));
     System.out.println("Expected [[4], [5, 6]] " + arrToString({4}, {5,6}));
     System.out.println("Expected [[-2, -3, 4], [5]] " + arrToString({-2,-3,4}, {5}));
     System.out.println("Expected [[-1], [5], [-3], [4], [7]] " + arrToString({-1}, {5}, {-3}, {4}, {7}));
     System.out.println("Expected [[10, 15, 8, -3, 0], []] " + arrToString({10, 15, 8, -3, 0}, {}));
-
+	
     System.out.println("Expected 40 " + arr2DSum({{10,5},{20,5}}));
     System.out.println("Expected -15 " + arr2DSum({{-5,-5},{-5}}));
     System.out.println("Expected 40 " + arr2DSum({{10,5},{20,5}}));
@@ -24,10 +24,23 @@ public class ArrayMethods{
   }
   public static String arrToString(int[][]ary){
   //this should use arrToString(int[])
-  return "";
+	String result - "[";
+	for (int i = 0; i < ary[].length;i++) {
+		result += arrayToString(ary[i][j]);
+		if (i != ary.length - 1) {
+			result += ", ";
+		}
+	}
+	return result + "]";
   }
+  
   public static int arr2DSum(int[][]nums){
-
-  return null;
+	int sum = 0;
+	for (int i = 0; i < nums[].length; i++) {
+		for (int j = 0; j < nums[i][].length; j++) {
+			sum += nums[i][j];
+		}
+	}
+	return sum;
   }
 }
