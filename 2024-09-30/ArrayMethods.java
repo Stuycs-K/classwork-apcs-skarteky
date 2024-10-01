1432f2213287ddf14d494f573e3c92c57660f78a
Selina Lin <selinalin2178@gmail.com>
2024-10-01 02:45:07 -0400

    added test cases to arrToString and arr2DSum

--- 2024-09-30/ArrayMethods.java
+++ 2024-09-30/ArrayMethods.java
@@ -5,12 +5,17 @@ public class ArrayMethods{
     System.out.println("Expected [[1, 2], [3]] " + arrToString(new int[][]{{1,2}, {3}}));
     System.out.println("Expected [[4], [5, 6]] " + arrToString(new int[][]{{4}, {5,6}}));
     System.out.println("Expected [[-2, -3, 4], [5]] " + arrToString(new int[][]{{-2,-3,4}, {5}}));
+	System.out.println("Expected [[-2, -3], [4, 5]] " + arrToString(new int[][]{{-2,-3}, {4,5}}));
     System.out.println("Expected [[-1], [5], [-3], [4], [7]] " + arrToString(new int[][]{{-1}, {5}, {-3}, {4}, {7}}));
-  System.out.println("Expected [[10, 15, 8, -3, 0], []] " + arrToString(new int[][]{{10, 15, 8, -3, 0}, {}}));
+	System.out.println("Expected [[10, 15, 8, -3, 0], []] " + arrToString(new int[][]{{10, 15, 8, -3, 0}, {}}));
 	
     System.out.println("Expected 40 " + arr2DSum(new int[][]{{10,5},{20,5}}));
     System.out.println("Expected -15 " + arr2DSum(new int[][]{{-5,-5},{-5}}));
     System.out.println("Expected 40 " + arr2DSum(new int[][]{{10,5},{20,5}}));
+	System.out.println("Expected 40 " + arr2DSum(new int[][]{{10,5},{20,5}}));
+	System.out.println("Expected 40 " + arr2DSum(new int[][]{{40,0},{}}));
+	System.out.println("Expected 40 " + arr2DSum(new int[][]{{10,15},{20,-5}}));
+	System.out.println("Expected 40 " + arr2DSum(new int[][]{{15},{5},{20}}));
   }
   public static String arrayToString(int[] nums) {
     String result = "[";
