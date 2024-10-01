f2dc580cbbc3762148e1a9f4b7f9196e54825af0
Selina Lin <selinalin2178@gmail.com>
2024-10-01 01:23:12 -0400

    fixed test cases for arrToString and arr2DSum

--- 2024-09-30/ArrayMethods.java
+++ 2024-09-30/ArrayMethods.java
@@ -2,15 +2,15 @@ public class ArrayMethods{
   /* Kevin Li kevinl687@nycstudents.net
      Selina Lin selinal50@nycstudents.net */
   public static void main(String args[]) {
-    System.out.println("Expected [[1, 2], [3]] " + arrToString({1,2}, {3}));
-    System.out.println("Expected [[4], [5, 6]] " + arrToString({4}, {5,6}));
-    System.out.println("Expected [[-2, -3, 4], [5]] " + arrToString({-2,-3,4}, {5}));
-    System.out.println("Expected [[-1], [5], [-3], [4], [7]] " + arrToString({-1}, {5}, {-3}, {4}, {7}));
-    System.out.println("Expected [[10, 15, 8, -3, 0], []] " + arrToString({10, 15, 8, -3, 0}, {}));
+    System.out.println("Expected [[1, 2], [3]] " + arrToString(new int[][]{{1,2}, {3}}));
+    System.out.println("Expected [[4], [5, 6]] " + arrToString(new int[][]{{4}, {5,6}}));
+    System.out.println("Expected [[-2, -3, 4], [5]] " + arrToString(new int[][]{{-2,-3,4}, {5}}));
+    System.out.println("Expected [[-1], [5], [-3], [4], [7]] " + arrToString(new int[][]{{-1}, {5}, {-3}, {4}, {7}}));
+  System.out.println("Expected [[10, 15, 8, -3, 0], []] " + arrToString(new int[][]{{10, 15, 8, -3, 0}, {}}));
 	
-    System.out.println("Expected 40 " + arr2DSum({{10,5},{20,5}}));
-    System.out.println("Expected -15 " + arr2DSum({{-5,-5},{-5}}));
-    System.out.println("Expected 40 " + arr2DSum({{10,5},{20,5}}));
+    System.out.println("Expected 40 " + arr2DSum(new int[][]{{10,5},{20,5}}));
+    System.out.println("Expected -15 " + arr2DSum(new int[][]{{-5,-5},{-5}}));
+    System.out.println("Expected 40 " + arr2DSum(new int[][]{{10,5},{20,5}}));
   }
   public static String arrayToString(int[] nums) {
     String result = "[";
@@ -24,9 +24,9 @@ public class ArrayMethods{
   }
   public static String arrToString(int[][]ary){
   //this should use arrToString(int[])
-	String result - "[";
-	for (int i = 0; i < ary[].length;i++) {
-		result += arrayToString(ary[i][j]);
+	String result = "[";
+	for (int i = 0; i < ary.length;i++) {
+		result += arrayToString(ary[i]);
 		if (i != ary.length - 1) {
 			result += ", ";
 		}
@@ -36,8 +36,8 @@ public class ArrayMethods{
   
   public static int arr2DSum(int[][]nums){
 	int sum = 0;
-	for (int i = 0; i < nums[].length; i++) {
-		for (int j = 0; j < nums[i][].length; j++) {
+	for (int i = 0; i < nums.length; i++) {
+		for (int j = 0; j < nums[i].length; j++) {
 			sum += nums[i][j];
 		}
 	}
