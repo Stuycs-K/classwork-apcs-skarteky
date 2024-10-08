58404f8310f5d6b1e28583c87d3b651eb930d197
Selina Lin <selinalin2178@gmail.com>
2024-10-07 23:43:32 -0400

    added test cases comparing built in and my version of toString

--- 2024-10-07-2DArrayMethods/ArrayDemo.java
+++ 2024-10-07-2DArrayMethods/ArrayDemo.java
@@ -1,17 +1,24 @@
 import java.util.Arrays;
 public class ArrayDemo {
     public static void main(String args[]) {
-      new int[] test1 = {};
+      int[] test1 = {};
       System.out.println("are Arrays.toString(test1) and arrToString(test1) the same: " + Arrays.toString(test1).equals(arrToString(test1)));
-      new int[] test2 = {1,2,3};
+      int[] test2 = {1,2,3};
       System.out.println("are Arrays.toString(test2) and arrToString(test2) the same: " + Arrays.toString(test2).equals(arrToString(test2)));
-      new int[] test3 = {-4,-5,-6};
+      int[] test3 = {-4,-5,-6};
       System.out.println("are Arrays.toString(test3) and arrToString(test3) the same: " + Arrays.toString(test3).equals(arrToString(test3)));
-      new int[] test4 = {-7,8,9,-10};
+      int[] test4 = {-7,8,9,-10};
       System.out.println("are Arrays.toString(test4) and arrToString(test4) the same: " + Arrays.toString(test4).equals(arrToString(test4)));
-      new int[][] test1 = {};
-      System.out.println("are Arrays.deepToString(test) and arrToString(test) the same: " + Arrays.deepToString(test).equals(arrToString(test)));
-    }
+      int[][] test5 = {};
+      System.out.println("are Arrays.deepToString(test5) and arrToString(test5) the same: " + Arrays.deepToString(test5).equals(arrToString(test5)));
+	  int[][] test6 = {{2,3},{-4,-4}};
+      System.out.println("are Arrays.deepToString(test6) and arrToString(test6) the same: " + Arrays.deepToString(test6).equals(arrToString(test6)));
+	  int[][] test7 = {{1},{2,3},{4,5,6}};
+      System.out.println("are Arrays.deepToString(test7) and arrToString(test7) the same: " + Arrays.deepToString(test7).equals(arrToString(test7)));
+	  int[][] test8 = {{},{-1},{3,4}};
+      System.out.println("are Arrays.deepToString(test8) and arrToString(test8) the same: " + Arrays.deepToString(test8).equals(arrToString(test8)));
+	
+	}
     public static String arrToString(int[] nums) {
       String result = "[";
       for(int i = 0; i < nums.length; i++) {
@@ -26,7 +33,7 @@ public class ArrayDemo {
     //this should use arrToString(int[])
   	String result = "[";
   	for (int i = 0; i < ary.length;i++) {
-  		result += arrayToString(ary[i]);
+  		result += arrToString(ary[i]);
   		if (i != ary.length - 1) {
   			result += ", ";
   		}
