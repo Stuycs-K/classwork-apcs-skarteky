c38a3d47a41ac141264e75a6c28146e49207e4a1
Selina Lin <selinalin2178@gmail.com>
2024-11-07 11:46:03 -0500

    answered step 10

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,7 +1,8 @@
 //8. What is the difference before and after you override this method? What does this tell us about methods that are overridden?
 //After overriding the method, only the bird constructor uses the overridden method: the original Animal constructor still uses the old method.
 //This tells us that if you override a method in a subclass, you can still access the origianal method.
-
+//Only the Bird storing an Animal didn't work.
+// This is likely because animal doesn't have all the parameters needed to create an instance of a Bird.
 public class Driver{
   public static void main(String[] args) {
     Animal cat = new Animal ("meow", 2, "green mario");
@@ -10,7 +11,7 @@ public class Driver{
     dog.speak();
     Bird bird = new Bird ("tweet", 2, "pink daisy", 100, "pink");
     bird.speak();
-    Bird bird2 = new Animal("squack", 2, "yellow peach");
+    //Bird bird2 = new Animal("squack", 2, "yellow peach");
     Animal dinosaur = new Bird("caw caw", 100000000, "mark zuckerberg", 4, "green");
   }
 }
