9a80784ee5de59980b59a810959bf334869cb91d
Selina Lin <selinalin2178@gmail.com>
2024-11-07 11:42:43 -0500

    added constructors, speak, in bird overrode speak

---
+++ 2024-11-07-Pets/Driver.java
@@ -0,0 +1,16 @@
+//8. What is the difference before and after you override this method? What does this tell us about methods that are overridden?
+//After overriding the method, only the bird constructor uses the overridden method: the original Animal constructor still uses the old method.
+//This tells us that if you override a method in a subclass, you can still access the origianal method.
+
+public class Driver{
+  public static void main(String[] args) {
+    Animal cat = new Animal ("meow", 2, "green mario");
+    Animal dog = new Animal ("woof", 2, "red luigi");
+    cat.speak();
+    dog.speak();
+    Bird bird = new Bird ("tweet", 2, "pink daisy", 100, "pink");
+    bird.speak();
+    Bird bird2 = new Animal("squack", 2, "yellow peach");
+    Animal dinosaur = new Bird("caw caw", 100000000, "mark zuckerberg", 4, "green");
+  }
+}
