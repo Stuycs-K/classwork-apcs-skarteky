81e27ce88405c6d8571f763fd86cad274bf035f6
Selina Lin <selinalin2178@gmail.com>
2024-11-20 11:46:32 -0500

    started day 4

--- 2024-11-18-advent2016/Code.java
+++ 2024-11-18-advent2016/Code.java
@@ -5,9 +5,9 @@ import java.io.FileNotFoundException;
 public class Code{
 
 
-  public static int getNum(String input){
-    int positionX = 1;
-    int positionY = 1;
+  public static int getNum(String input, int x, int y){
+    int positionX = x;
+    int positionY = y;
     int[][] keypad = {{1,2,3}, {4,5,6}, {7,8,9}};
     int i = 0;
     while(i < input.length()){
@@ -28,13 +28,63 @@ public class Code{
     }
     return keypad[positionY][positionX];
   }
+  public static int getX(String input, int x, int y){
+    int positionX = x;
+    int positionY = y;
+    int[][] keypad = {{1,2,3}, {4,5,6}, {7,8,9}};
+    int i = 0;
+    while(i < input.length()){
+      char nextMove = input.charAt(i);
+      if(nextMove == 'L'){
+        if(positionX != 0) positionX--;
+      }
+      if(nextMove == 'R'){
+        if(positionX != 2)positionX++;
+      }
+      if(nextMove == 'D'){
+        if(positionY != 0)positionY--;
+      }
+      if(nextMove == 'U'){
+        if(positionY != 2)positionY++;
+      }
+      i++;
+    }
+    return positionX;
+  }
+  public static int getY(String input, int x, int y){
+    int positionX = x;
+    int positionY = y;
+    int[][] keypad = {{1,2,3}, {4,5,6}, {7,8,9}};
+    int i = 0;
+    while(i < input.length()){
+      char nextMove = input.charAt(i);
+      if(nextMove == 'L'){
+        if(positionX != 0) positionX--;
+      }
+      if(nextMove == 'R'){
+        if(positionX != 2)positionX++;
+      }
+      if(nextMove == 'D'){
+        if(positionY != 0)positionY--;
+      }
+      if(nextMove == 'U'){
+        if(positionY != 2)positionY++;
+      }
+      i++;
+    }
+    return positionY;
+  }
   public static String findCode(String filename){
     String code = "";
+    int x = 1;
+    int y = 1;
     try{File file = new File(filename);
     Scanner input = new Scanner(file);
     while(input.hasNextLine()){
       String in = input.nextLine();
-      code+=getNum(in);
+      code+=getNum(in, x, y);
+      x = getX(in, x, y);
+      y = getY(in, x, y);
     }
     return code;
     }catch(FileNotFoundException e){
---
+++ 2024-11-18-advent2016/Security.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Security {
+  public static int[] countCommon(String in){
+    int[] letters = new int[26];
+    for(int i - 0; i < 26; i++){
+      Scanner input = new Scanner(in);
+
+    }
+  }
+
+  public static void main(String[] args){
+
+  }
+}
