98e016baaaff259865e95d37f582ba31244a87fd
Selina Lin <selinalin2178@gmail.com>
2024-12-03 11:57:35 -0500

    continued testing colors and positions

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,20 +1,29 @@
 public class ColorDemo{
   public static final String CLEAR_SCREEN =  "\u001b[2J";
-public static final String HIDE_CURSOR =  "\u001b[?25l";
-public static final String SHOW_CURSOR =  "\u001b[?25h";
-public static int BLACK = 30;
-public static int RED = 31;
+  public static final String HIDE_CURSOR =  "\u001b[?25l";
+  public static final String SHOW_CURSOR =  "\u001b[?25h";
+  public static int BLACK = 30;
+  public static int RED = 31;
 
-public static void color(int foreground,int background){
-  System.out.print( "\u001b[" + foreground + ";" + (10+background) + "m");
-}
+  public static void color(int foreground,int background){
+    System.out.print( "\u001b[" + foreground + ";" + (10+background) + "m");
+  }
 
-public static void color(int foreground, int background, int modifier){
-  System.out.print( "\u001b[" + foreground + ";" + (10+background) + ";" + modifier + "m");
-}
+  public static void color(int foreground, int background, int modifier){
+    System.out.print( "\u001b[" + foreground + ";" + (10+background) + ";" + modifier + "m");
+  }
+  public static void go(int r,int c){
+    System.out.print("\u001b[" + r + ";" + c + "f");
+  }
 
   public static void main(String[] args){
     color(32,33);
-    
+    System.out.println("meow");
+    color(34,35,4);
+    System.out.println("meow");
+    go(1, 1);
+    System.out.println("meow");
+
+
   }
 }
