a683a5bc0c7380b7a20f7d3db9de8d5f7c280a97
Selina Lin <selinalin2178@gmail.com>
2024-12-04 11:21:11 -0500

    started border

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,5 +1,12 @@
 public class Driver{
   public static void main(String[] args){
+    for(int i = 0; i < 40; i++){
+      Text.color(Text.background(Text.RED));
+      System.out.println(".");
+      Text.color(Text.background(Text.BLUE));
+      System.out.println(".");
 
+    }
+    Text.color(Text.RESET);
   }
 }
