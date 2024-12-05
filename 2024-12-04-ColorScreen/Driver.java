205f95067bafb0a9f801b0e102ccdd3b54359871
Selina Lin <selinalin2178@gmail.com>
2024-12-05 03:26:05 -0500

    finished border

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -2,11 +2,42 @@ public class Driver{
   public static void main(String[] args){
     for(int i = 0; i < 40; i++){
       Text.color(Text.background(Text.RED));
-      System.out.println(".");
+      System.out.print(" ");
       Text.color(Text.background(Text.BLUE));
-      System.out.println(".");
-
+      System.out.print(" ");
     }
-    Text.color(Text.RESET);
+    System.out.println(Text.RESET);
+    for(int i = 0; i < 15; i++) {
+    	Text.color(Text.background(Text.BLUE));
+        System.out.print(" ");
+        System.out.print(Text.RESET);
+        for(int j = 0; j < 78; j++) {
+        	System.out.print(" ");
+        }
+        Text.color(Text.background(Text.RED));
+        System.out.println(" ");
+        
+        Text.color(Text.background(Text.RED));
+        System.out.print(" ");
+        System.out.print(Text.RESET);
+        for(int j = 0; j < 78; j++) {
+        	System.out.print(" ");
+        }
+        Text.color(Text.background(Text.BLUE));
+        System.out.print(" ");
+        System.out.println(Text.RESET);
+    }
+    for(int i = 0; i < 40; i++){
+        Text.color(Text.background(Text.BLUE));
+        System.out.print(" ");
+        Text.color(Text.background(Text.RED));
+        System.out.print(" ");
+      }
+    Text.go(2, 1);
+    int[] random = new int[3]
+    
+    
+    
+    System.out.println(Text.RESET);
   }
 }
