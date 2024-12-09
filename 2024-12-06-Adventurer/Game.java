4836cd87c39779c1edea237d8da6a26418347ab3
Selina Lin <selinalin2178@gmail.com>
2024-12-09 11:46:14 -0500

    added to battle sequence in Game.java

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -16,5 +16,30 @@ public class Game{
     CodeWarrior opp = new CodeWarrior(oppName);
     //Do something with the input
     System.out.println("Opponent is: " + oppName);
+
+    int turn = 1;
+    System.out.println("Battle Start");
+    while(user.getHP() != 0 && opp.getHP() != 0){
+      System.out.println("Turn " + turn);
+
+      System.out.println(user.toString() + " - " + user.getHP() + "/" + user.getmaxHP() + " - " + user.getSpecial() + "/" + user.getSpecialMax() + " " + user.getSpecialName());
+      System.out.println(opp.toString() + " - " + opp.getHP() + "/" + opp.getmaxHP() + " - " + opp.getSpecial() + "/" + opp.getSpecialMax() + " " + opp.getSpecialName());
+      System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
+      String move = userInput.nextLine();
+      if(move.equals("a") || move.equals("attack")){
+        user.attack(opp);
+      }
+      if(move.equals("sp") || move.equals("special")){
+        user.attack(opp);
+      }
+      if(move.equals("su") || move.equals("support")){
+        user.attack(opp);
+      }
+      if(move.equals("quit")){
+        user.setHP(0);
+        opp.setHP(0);
+      }
+
+    }
   }
 }
