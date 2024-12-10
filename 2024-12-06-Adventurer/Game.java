adfcb1bfbee1340e985e6d2e19b495fa4b6db986
Selina Lin <selinalin2178@gmail.com>
2024-12-10 01:30:42 -0500

    added to battle sequence

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -19,27 +19,59 @@ public class Game{
 
     int turn = 1;
     System.out.println("Battle Start");
-    while(user.getHP() != 0 && opp.getHP() != 0){
+    while(user.getHP() > 0 && opp.getHP() > 0){
       System.out.println("Turn " + turn);
 
-      System.out.println(user.toString() + " - " + user.getHP() + "/" + user.getmaxHP() + " - " + user.getSpecial() + "/" + user.getSpecialMax() + " " + user.getSpecialName());
-      System.out.println(opp.toString() + " - " + opp.getHP() + "/" + opp.getmaxHP() + " - " + opp.getSpecial() + "/" + opp.getSpecialMax() + " " + opp.getSpecialName());
+      System.out.println(user.toString() + " - " + user.getHP() + "/" + user.getmaxHP() + "HP - " + user.getSpecial() + "/" + user.getSpecialMax() + " " + user.getSpecialName());
+      System.out.println(opp.toString() + " - " + opp.getHP() + "/" + opp.getmaxHP() + "HP - " + opp.getSpecial() + "/" + opp.getSpecialMax() + " " + opp.getSpecialName());
       System.out.println("Type: (a)ttack / (sp)ecial / (su)pport / quit");
       String move = userInput.nextLine();
+     
       if(move.equals("a") || move.equals("attack")){
-        user.attack(opp);
+        System.out.println(user.attack(opp));
       }
       if(move.equals("sp") || move.equals("special")){
-        user.attack(opp);
+    	  System.out.println(user.specialAttack(user));
       }
       if(move.equals("su") || move.equals("support")){
-        user.attack(opp);
+    	  System.out.println("Support 1 or 2");
+    	  String choose = userInput.nextLine();
+    	  if(choose.equals("1")) {
+    		  System.out.println(user.support()); 
+    	  }else {
+    		  System.out.println(user.support(user));
+    	  }
       }
       if(move.equals("quit")){
         user.setHP(0);
         opp.setHP(0);
+        System.out.println("Game Quit.");
+        break;
       }
+      int temp = (int)(Math.random() * 3);
+      if(temp == 0) {
+    	  System.out.println(opp.attack(user));
+      }
+      if(temp == 1) {
+    	  int temp2 = (int)(Math.random() * 2);
+    	  if(temp2 == 0) {
+    		  System.out.println(opp.support());
+    	  }else {
+    		  System.out.println(opp.support(opp));
+    	  }
+      }
+      if(temp == 2) {
+    	  System.out.println(opp.specialAttack(user));
+      }
+      turn++;
 
     }
+    String loser = "";
+    if(user.getHP() < 1) {
+    	loser = userName;
+    }else {
+    	loser = oppName;
+    }
+    System.out.println("Game over! " + loser + " loses!");
   }
 }
