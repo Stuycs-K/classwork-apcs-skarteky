9aa4ccdd62fb760ed88fa92dd7cc398b41421337
Selina Lin <selinalin2178@gmail.com>
2025-01-03 11:43:02 -0500

    started toWords

--- 2025-01-02/Words.java
+++ 2025-01-02/Words.java
@@ -9,7 +9,51 @@ public class Words{
     }
   }
 
+  public static String toWords(int n){
+    if(n < 12){
+      if(n== 12){
+        return "twelve";
+      }
+      if(n== 11){
+        return "eleven";
+      }
+      if(n==10){
+        return "ten";
+      }
+      if(n==9){
+        return "nine";
+      }
+      if(n==8){
+        return "eight";
+      }
+      if(n==7){
+        return "seven";
+      }
+      if(n==6){
+        return "six";
+      }
+      if(n==5){
+        return "five";
+      }
+      if(n==4){
+        return "four";
+      }
+      if(n==3){
+        return "three";
+      }
+      if(n==2){
+        return "two";
+      }
+      if(n==1){
+        return "one";
+      }
+      if(n==0){
+        return "zero";
+      }
+    }
+  }
+
   public static void main(String args[]){
-    makeWords(3, "", "abc");
+    makeWords(3, "", "xy");
   }
 }
