6fa52e4223bd83819137cf20fcd9af7b951d8812
Selina Lin <selinalin2178@gmail.com>
2025-01-03 11:37:52 -0500

    finished makeWords added testing

--- 2025-01-02/Words.java
+++ 2025-01-02/Words.java
@@ -1,5 +1,15 @@
 public class Words{
   public static void makeWords(int remainingLetters, String result, String alphabet){
-    
+    if(remainingLetters == 0){
+      System.out.println(result);
+    }else {
+      for(int i = 0; i < alphabet.length(); i++){
+        makeWords(remainingLetters-1, result + alphabet.charAt(i), alphabet);
+      }
+    }
+  }
+
+  public static void main(String args[]){
+    makeWords(3, "", "abc");
   }
 }
